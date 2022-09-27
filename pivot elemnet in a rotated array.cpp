ebab731f6d754fc72f80dde145ab174880e8cdce
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-09-27 13:49:18 +0530

    Add files via upload

---
+++ pivot elemnet in a rotated array.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int pivotnumber(int arr[],int n)
+{
+	int s=0,e=n-1;
+	int m=s+(e-s)/2;
+	while(s<e)
+	{
+		if(arr[m]>=arr[0])
+		{
+			s=m+1;
+		}
+		else
+		{
+			e=m;
+		}
+		m=s+(e-s)/2;
+	}
+	return arr[s];
+}
+int main()
+{
+	int arr[10],n,i;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<pivotnumber(arr,n);
+}
